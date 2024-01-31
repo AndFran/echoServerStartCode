28314954148e7a606576167f5cb4ad86a41384b6
AndFran <andrewwilliamfranks@gmail.com>
2024-01-31 09:14:35 +0100

    Create client.go

---
+++ internal/database/client.go
@@ -0,0 +1,44 @@
+package database
+
+import (
+	"fmt"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
+	"time"
+)
+
+type DatabaseClient interface {
+	Ready() bool
+}
+
+// Client -> wrap a gorm instance and supply an interface DatabaseClient
+type Client struct {
+	DB *gorm.DB
+}
+
+func (c Client) Ready() bool {
+	var ready string
+	tx := c.DB.Raw("SELECT 1 as ready").Scan(&ready)
+	if tx.Error != nil {
+		return false
+	}
+	return ready == "1"
+}
+
+// return an interfac
+func NewDatabaseClient() (DatabaseClient, error) {
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=%s",
+		"localhost", "postgres", "Rr1234!!", "wisdom", 5432, "disable")
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{NamingStrategy: schema.NamingStrategy{
+		TablePrefix: "wisdom.",
+	},
+		NowFunc:     func() time.Time { return time.Now().UTC() },
+		QueryFields: true})
+	if err != nil {
+		return nil, err
+	}
+	client := Client{DB: db}
+	return client, nil
+}
